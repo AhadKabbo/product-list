e8471646b1d4a6efd313292e3042e70953de0828
Mohammad Ahad Hossain Kabbo <ahadhossainkabbo@gmail.com>
2024-01-28 22:29:34 +0600

    run hello world

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* <Header /> */}
+        <nav>
+          <ul className="flex gap-6">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/cart">Cart</Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+        {children}
+      </body>
     </html>
   );
 }
