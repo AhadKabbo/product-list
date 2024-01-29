57f7728384c920229dfa31c5a2c886ed2a7fe27e
Mohammad Ahad Hossain Kabbo <ahadhossainkabbo@gmail.com>
2024-01-29 16:19:39 +0600

    tailwind change

--- app/page.jsx
+++ app/page.jsx
@@ -5,18 +5,19 @@ import Link from "next/link";
 const HomePage = async () => {
   const products = (await getData("https://fakestoreapi.com/products")) ?? [];
   return (
-    <div className=" px-20 py-16">
-      <h2 className="text-4xl font-bold p-4">Shopping Site</h2>
-      <div className="bg-white border border-gray-300 rounded-lg  dark:bg-gray-700 dark:border-gray-700 text-slate-800 overflow-hidden">
-        <div className="bg-slate-100 dark:bg-gray-800 py-3 px-6 font-semibold border-b border-gray-300 dark:border-gray-600 text-slate-800 dark:text-slate-100 flex justify-between items-center">
-          <h2>Products</h2>
+    <div className="bg-gray-600 mx-auto my-3 max-w-7xl px-2 py-8 rounded-md sm:px-6 lg:px-8">
+      <div className="border border-gray-300 rounded-lg  dark:bg-gray-700 dark:border-gray-700 text-slate-800 overflow-hidden">
+        <div className="bg-amber-50 dark:bg-gray-800 py-3 px-6 font-semibold border-b border-gray-300 dark:border-gray-600 text-slate-800 dark:text-slate-100 flex justify-between items-center">
+          <h2 className="text-2xl  py-2 px-3 rounded-lg uppercase text-[#A4BC46] font-semibold">
+            Products
+          </h2>
           <Link
-            className="bg-lime-600 hover:bg-lime-800 duration-300 transition-all text-slate-50 rounded-md px-4 py-2"
+            className="bg-gradient-to-r from-[#A4BC46] to-[#85A019] py-2 px-3 rounded-lg uppercase text-white font-semibold"
             href="/cart">
             View Cart (<CartLength />)
           </Link>
         </div>
-        <div className="bg-white dark:bg-slate-700 p-4">
+        <div className="bg-white dark:bg-slate-700 px-4 py-6">
           <ProductList products={products} />
         </div>
       </div>
--- components/Breadcrumb.jsx
+++ components/Breadcrumb.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Breadcrumb() {
   return (
-    <div className="bg-amber-100 mx-auto my-3 max-w-7xl px-2 rounded-md sm:px-6 lg:px-8">
+    <div className="bg-gray-600  mx-auto my-3 max-w-7xl px-2 rounded-md sm:px-6 lg:px-8">
       <nav
         className="flex md:justify-around md:-space-x-40 py-6 px-8 md:px-10"
         aria-label="Breadcrumb">
@@ -12,7 +12,7 @@ export default function Breadcrumb() {
           <li className="inline-flex items-center">
             <Link
               href="/"
-              className="px-7 py-3 inline-flex items-center text-2xl font-medium text-gray-700 hover:text-[#a2c907] dark:text-gray-400 dark:hover:text-white">
+              className="px-7 py-3 inline-flex items-center text-2xl font-medium text-amber-100  hover:text-[#a2c907] dark:text-gray-400 dark:hover:text-white">
               <Home className="w-5 h-5 me-2.5" />
               Home
             </Link>
@@ -21,7 +21,7 @@ export default function Breadcrumb() {
             <div className="flex items-center">
               <Link
                 href="/cart"
-                className="px-7 py-3 inline-flex items-center text-2xl font-medium text-gray-700 hover:text-[#a2c907] dark:text-gray-400 dark:hover:text-white">
+                className="px-7 py-3 inline-flex items-center text-2xl font-medium text-amber-100 hover:text-[#a2c907] dark:text-gray-400 dark:hover:text-white">
                 My Cart
               </Link>
             </div>
