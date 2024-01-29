a9047a4a3ea9cd6c5bed967550577690ebc1d8ca
Mohammad Ahad Hossain Kabbo <ahadhossainkabbo@gmail.com>
2024-01-29 15:36:22 +0600

    Navbar tailwind redesign

--- components/Breadcrumb.jsx
+++ components/Breadcrumb.jsx
@@ -4,35 +4,30 @@ import React from "react";
 
 export default function Breadcrumb() {
   return (
-    <nav className="flex mb-2" aria-label="Breadcrumb">
-      <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-        <li className="inline-flex items-center">
-          <Link
-            href="/"
-            className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-            <Home className="w-3 h-3 me-2.5" />
-            Home
-          </Link>
-        </li>
-        <li>
-          <div className="flex items-center">
-            <ChevronRight className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" />
+    <div className="bg-amber-100 mx-auto my-3 max-w-7xl px-2 rounded-md sm:px-6 lg:px-8">
+      <nav
+        className="flex md:justify-around md:-space-x-40 py-6 px-8 md:px-10"
+        aria-label="Breadcrumb">
+        <ol className="inline-flex items-center ">
+          <li className="inline-flex items-center">
             <Link
-              href="/cart"
-              className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">
-              My Cart
+              href="/"
+              className="px-7 py-3 inline-flex items-center text-2xl font-medium text-gray-700 hover:text-[#a2c907] dark:text-gray-400 dark:hover:text-white">
+              <Home className="w-5 h-5 me-2.5" />
+              Home
             </Link>
-          </div>
-        </li>
-        {/* <li aria-current="page">
-          <div className="flex items-center">
-            <ChevronRight className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" />
-            <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
-              Flowbite
-            </span>
-          </div>
-        </li> */}
-      </ol>
-    </nav>
+          </li>
+          <li>
+            <div className="flex items-center">
+              <Link
+                href="/cart"
+                className="px-7 py-3 inline-flex items-center text-2xl font-medium text-gray-700 hover:text-[#a2c907] dark:text-gray-400 dark:hover:text-white">
+                My Cart
+              </Link>
+            </div>
+          </li>
+        </ol>
+      </nav>
+    </div>
   );
 }
